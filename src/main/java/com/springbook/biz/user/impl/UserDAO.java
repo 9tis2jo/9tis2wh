9383d97fb28ab08e6a4445f86b0bbff2b20c38b3
waterbeam <92918520+waterbeam@users.noreply.github.com>
2022-03-10 18:21:03 +0900

    Add files via upload

--- src/main/java/com/springbook/biz/user/impl/UserDAO.java
+++ src/main/java/com/springbook/biz/user/impl/UserDAO.java
@@ -1,6 +1,7 @@
 package com.springbook.biz.user.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -16,6 +17,8 @@ public class UserDAO {
 	private ResultSet rs = null;
 	
 	private final String USER_GET = "select * from Member where mID=? and mPW=?";
+	private final String USER_UPDATE = "update user set mID = ?, mPW = ?,"
+			+ "mNAME = ? , mBIR = ? , mPHONE = ? , mEMAIL = ? , mPOST = ? , mADDR = ?";
 	
 	public UserVO getUser(UserVO vo) {
 		
@@ -51,6 +54,30 @@ public class UserDAO {
 		}
 		
 		return user;
+	}
+
+	public void updateInfo(UserVO vo) {
+		System.out.println("====> JDBC로 updatePRODUCT() 기능 처리");
+		
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(USER_UPDATE);
+			
+			stmt.setString(1, vo.getMID());
+			stmt.setString(2, vo.getMPW());
+			stmt.setString(3, vo.getMNAME());
+			stmt.setDate(4, (Date) vo.getMBIR());
+			stmt.setString(5, vo.getMPHONE());
+			stmt.setString(6, vo.getMEMAIL());
+			stmt.setString(7, vo.getMPOST());
+			stmt.setString(8, vo.getMADDR());
+			stmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(stmt, conn);
+		}
+		
 	}
 }
 
